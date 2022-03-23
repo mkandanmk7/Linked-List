427efda1f895355c457ad263cfee63cd44b0126d
mkandanmk7 <mkandanmk7@gmail.com>
2022-03-23 08:05:20 +0530

    post order done

--- src/pages/Post.jsx
+++ src/pages/Post.jsx
@@ -4,11 +4,13 @@ import React, { useEffect, useState } from 'react'
 const Post = () => {
     const [post, setPost] = useState({});
     const [allPosts, setAllPosts] = useState([]);
+    console.log("allposts:", allPosts);
 
     const getAllPosts = async () => {
         try {
             const allPost = await axios.get(`http://localhost:4001/posts/allposts`);
-            setAllPosts(allPost);
+            console.log("all posts:", allPost);
+            setAllPosts(allPost.data);
         } catch (err) {
             console.log(err.message);
         }
@@ -16,6 +18,7 @@ const Post = () => {
     const length = allPosts.length;
 
     const lastPost = allPosts[length - 1];
+    console.log("lastPost:", lastPost);
 
     const disablePrevButton = (post) => {
         if (post.prev === null) return true;
@@ -30,15 +33,20 @@ const Post = () => {
     }
 
     const getSinglePost = async (id) => {
-        // http://localhost:4001/posts
-        const postDetails = await axios.get(`http://localhost:4001/posts/${id}`);
-        console.log('postDetails :>> ', postDetails);
-        setPost(postDetails.data);
+        try {
+            const postDetails = await axios.get(`http://localhost:4001/posts/${id}`);
+            console.log('postDetails :>> ', postDetails);
+            setPost(postDetails.data);
+        } catch (err) {
+            console.log(err.message);
+        }
+
     }
 
-
+    // getSinglePost(lastPost.postId)
     useEffect(() => {
         getSinglePost(lastPost.postId);
+        // eslint-disable-next-line react-hooks/exhaustive-deps
     }, []);
 
     useEffect(() => {
@@ -54,17 +62,29 @@ const Post = () => {
         getSinglePost(post.prev);
     }
 
+    const deletePost = async (post) => {
+        try {
+            await axios.delete(`http://localhost:4001/posts/${post.postId}`);
+            console.log("post deleted");
+        } catch (error) {
+            console.log(error.message);
+        }
 
+
+    }
 
     return (
         <div className='container d-flex align-items-center  justify-content-center bg-light flex-column bg-light' style={{ height: "100vh" }}>
-            <div className='p-3'>
+            <div className='p-3 bg-secondary rounded'>
                 <h3 className="p-3 mt-3">title: {post.title}</h3>
-                <h5 className='text-secondary p-3'>Description: {post.description}</h5>
+                <h5 className='text-light p-3'>Description: {post.description}</h5>
                 {/* <p>Created At : {" " + new Date(post.createAt)}</p> */}
-                <div className="d-flex justify-content-around" >
+                <div className="d-flex justify-content-evenly" >
+
 
                     <button className='btn btn-success' disabled={disablePrevButton(post)} onClick={() => getPreviousPost(post)}>previous</button>
+                    <button className='btn btn-danger' onClick={() => deletePost(post)}>delete</button>
+
                     <button className='btn btn-success  ' disabled={disableNextButton(post)} onClick={() => getNextPost(post)}>next</button>
                 </div>
             </div>
